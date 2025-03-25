860e40a64c66823af7465d8dcdcf1ba3910ba942
humblegod1 <btrglm@proton.me>
2025-03-25 08:29:34 -0600

    :(

--- lua/kickstart/plugins/darkvoid.lua
+++ lua/kickstart/plugins/darkvoid.lua
@@ -3,12 +3,12 @@ return {
   lazy = false,
   priority = 1000,
   -- vim.cmd.colorscheme 'darkvoid',
-  config = function()
-    require('nvim-highlight-colors').setup {
-      -- Add your configuration options here (if any)
-      render = 'background', -- or 'foreground' or 'first_column'
-      enable_named_colors = true, -- Enable named colors support
-      enable_tailwind = true,
-    }
-  end,
+  -- config = function()
+  -- require('nvim-highlight-colors').setup {
+  -- Add your configuration options here (if any)
+  -- render = 'background', -- or 'foreground' or 'first_column'
+  -- enable_named_colors = true, -- Enable named colors support
+  -- enable_tailwind = true,
+  -- }
+  -- end,
 }
